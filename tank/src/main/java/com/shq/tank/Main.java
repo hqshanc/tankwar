0459372d4012cb829aa6b7c8952781b49fe300bf
hqshanc <847132441@qq.com>
2021-08-04 21:13:18 +0800

    配置文件使用

--- tank/src/main/java/com/shq/tank/Main.java
+++ tank/src/main/java/com/shq/tank/Main.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame t = new TankFrame();
-
+        int badCount = Integer.parseInt(PropertyMgr.get("initTankCount"));
         //初始化敌方坦克
-        for (int i = 0; i< 5; i++) {
+        for (int i = 0; i< badCount; i++) {
             t.tanks.add(new Tank(new Random().nextInt(1000), new Random().nextInt(1000), Dir.DOWN, t,Group.BAD, 3));
         }
         //循环调用刷新页面
